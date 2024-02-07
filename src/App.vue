fc450c6bd2eb462708f6f9dfa40268d75faaa004
salvoPernicano <salvo.pernicano@gmail.com>
2024-02-08 00:14:09 +0100

    feat icone shop

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,4 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss' ;
-
-
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,28 @@ export default {
             <h1>--Content goes here--</h1>
         </div>
         <div class="lowerMain">
-            <h1>Lorem, ipsum dolor.</h1>
+          <ul>
+            <li>
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <span>Dc merchandie</span>
+            </li>
+            <li>
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <span>Dc merchandie</span>
+            </li>
+            <li>
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <span>Dc merchandie</span>
+            </li>
+            <li>
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <span>Dc merchandie</span>
+            </li>
+            <li>
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <span>Dc merchandie</span>
+            </li>
+          </ul>
         </div>
 
     </main>
@@ -31,8 +52,26 @@ export default {
 .lowerMain{
     background-color: $siteBlue;
     text-align: center;
-    padding: 3em;
+    display: flex;
+    justify-content: center;
     height: 50%;
+  ul{
+    display: flex;
+    align-items: center;
+    gap: 50px;
+    color: white;
+    text-transform: uppercase;
+    list-style: none;
+    li{
+        display: flex;
+        align-items: center;
+        img{
+            width: 50px;
+            height: 50px;
+            object-fit: contain;
+        }
+    }
+  }
 }
 h1{
     color: white;
