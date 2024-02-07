58e93db40672b552ad1905421f0eaad95db7e567
salvoPernicano <salvo.pernicano@gmail.com>
2024-02-07 22:37:49 +0100

    feat base main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script>
-import AppNavbar from './components/AppNavbar.vue'
+import AppNavbar from './components/AppNavbar.vue';
+import AppMain from './components/AppMain.vue'
 export default {
     components: {
-        AppNavbar
+        AppNavbar,
+        AppMain
     }
 }
 </script>
 
 <template>
     <AppNavbar />
+    <AppMain/>
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' ;
+body{
+    background-color: aqua;
+    height: 100vh;
+}
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    name : 'AppMain',
+}
+</script>
+
+<template>
+    <main>
+        <div class="upperMain">
+            <h1>--Content goes here--</h1>
+        </div>
+        <div class="lowerMain">
+            <h1>Lorem, ipsum dolor.</h1>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@import '../styles/partials/variables';
+.upperMain{
+    background-color: $siteBlack;
+    text-align: center;
+    padding: 3em;
+}
+.lowerMain{
+    background-color: $siteBlue;
+    text-align: center;
+    padding: 3em;
+}
+h1{
+    color: white;
+}
+</style>
